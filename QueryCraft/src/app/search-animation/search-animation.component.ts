95d37353dfe482c077baec2500dc9bc62e598d9f
Alex Freer <chasealex6@hotmail.com>
2023-02-11 19:41:55 -0500

    added skip on type animation

--- QueryCraft/src/app/search-animation/search-animation.component.ts
+++ QueryCraft/src/app/search-animation/search-animation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-animation',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchAnimationComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  skipTypieType: boolean
 
-  ngOnInit(): void {
+  constructor() {
+    this.skipTypieType = false
+  }
+
+  ngOnInit (): void {
   }
 
 }
