20b7111716c2ba0ebce9a8eaefbfac4f61894163
Alex Freer <chasealex6@hotmail.com>
2023-02-12 10:45:32 -0500

    layout change

--- QueryCraft/src/app/app-routing.module.ts
+++ QueryCraft/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
-import { TutorialsComponent } from './tutorials/tutorials.component';
+// import { TutorialsComponent } from './tutorials/tutorials.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }, { path: 'tutorials', component: TutorialsComponent }];
+const routes: Routes = [{ path: '', component: HomepageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
