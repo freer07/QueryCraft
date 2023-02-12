3f3b153aa63f006e11ed8dc6ca2256eb88d17243
Joel Jacob <69165450+Joel-Liju@users.noreply.github.com>
2023-02-11 21:34:19 -0500

    changes added

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,48 @@
-from flask import Flask
-from pipeline import queryBuilder
 
-app = Flask(__name__)# the first set up
+# A very simple Flask Hello World app for you to get started with...
 
-@app.route("/userinput",methods=['POST'])
-    return queryBuilder(json.loads(request.data)['request'])
+from flask import Flask, json, request
+app = Flask(__name__)
+import cohere
 
-@app.route("/")
-def serverRunner():
-    return "<p>test test</p>"
+from cohere.classify import Example
+
+examples=[
+  Example("How old is my son?", "bad"),
+  Example("Mechanics nearby", "good"),
+  Example("Why the heck do I can think", "bad"),
+  Example("What is the meaning of existence?", "good"),
+  Example("where can I find the closest treasure?","bad"),
+  Example("Why did my car started making weird noises?","bad"),
+  Example("Where can find a place to crash tonight?","good"),
+  Example("what is a wrench useful for?","bad"),
+  Example("How far is Toronto?", "good"),
+  Example("What can I do today in my town?","good"),
+  Example("How can I copy paste in vim?","good"),
+  Example("Why is Vim not working?","bad"),
+  Example("Why does my heart hurt?","bad"),
+  Example("Why do I crave death?","bad"),
+  Example("My headaches is getting worse, where can I find help?","good")
+]
+
+
+
+@app.route('/userinput', methods=['GET'])
+def queryBuild():
+    co = cohere.Client('')
+
+    #response = co.generate(
+    #    prompt = prompt,
+    #)
+
+    response = co.classify(
+    model='large',
+    inputs=["this is a test"],
+    examples=examples
+    )
+    return response.classifications
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
