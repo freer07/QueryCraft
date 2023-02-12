655c3d3f7408513dca591619db4d4bdab91e18d5
Alex Freer <chasealex6@hotmail.com>
2023-02-12 09:24:17 -0500

    got carousel working
    hard coded it

--- QueryCraft/src/app/carousel/carousel.component.ts
+++ QueryCraft/src/app/carousel/carousel.component.ts
@@ -7,29 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  @Input() 
-  slides: any;
-
-  currentSlide = 0; 
-
+  currentSlide = 0;
+  slides = 2;
 
   constructor() {
-    this.slides = []
-   }
-
-  ngOnInit(): void {
   }
 
-  onPreviousClick(){ 
-    const previous = this.currentSlide - 1; 
-    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous; 
+  ngOnInit (): void {
+  }
+
+  onPreviousClick () {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides - 1 : previous;
     console.log("Previous clicked, new current slide is: ", this.currentSlide);
   }
 
-  onNextClick(){ 
-    const next = this.currentSlide + 1; 
-    this.currentSlide = next === this.slides.length ? 0 : next; 
+  onNextClick () {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides ? 0 : next;
     console.log("Next clicked, new current slide is: ", this.currentSlide)
   }
-
 }
--- QueryCraft/src/app/tutorials/tutorials.component.ts
+++ QueryCraft/src/app/tutorials/tutorials.component.ts
@@ -8,19 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class TutorialsComponent implements OnInit {
   // content for the tutorial, in the order we want to display it 
   // this only works for a specific form of query -- which ... sucks but it does give us two different queries? 
-  public slides = [ 
-    {intro_text: "How do I ", 
-     vocab_text: "get to", 
-     keyword_text: "Toronto airport",
-     exit_text: "?"}, 
-     {intro_text: "How do I find ", 
-     vocab_text: "someone to fix my car ", 
-     keyword_text: "nearby",
-     exit_text: "?"}
-  ]
+  // public slides = [ 
+  //   {intro_text: "How do I ", 
+  //    vocab_text: "get to", 
+  //    keyword_text: "Toronto airport",
+  //    exit_text: "?"}, 
+  //    {intro_text: "How do I find ", 
+  //    vocab_text: "someone to fix my car ", 
+  //    keyword_text: "nearby",
+  //    exit_text: "?"}
+  // ]
+  // public slides = [
+  //   {
+  //     component: 'highlight',
+  //     inner: [{ componenet: 'text', text: 'How do I' }, { componenet: '' }]
+  //   }
+  // ]
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
   }
 
 }
